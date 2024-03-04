519f0a212a80fec85e990efc744c44dad78ddbda
Ben Anderson <andersjbe@gmail.com>
2024-03-04 11:42:39 -1000

    completed solution

--- index.js
+++ index.js
@@ -1,33 +1,33 @@
 let library = [
-    {
-        title: "A Game of Thrones",
-        tags: ["fantasy", "george r.r. martin"],
-    },
-    {
-        title: "Eloquent JavaScript",
-        tags: ["technology", "programming", "marijn haverbeke"],
-    },
-    {
-        title: "The Fellowship of the Ring",
-        tags: ["fantasy", "jrr tolkien"],
-    },
-    {
-        title: "The Return of the King",
-        tags: ["fantasy", "jrr tolkien"],
-    },
-    {
-        title: "The Anthropocene Reviewed",
-        tags: ["nonfiction", "john green"],
-    },
-    {
-        title: "The Left Hand of Darkness",
-        tags: ["sci-fi", "ursula le guin "],
-    },
-    // add more books if you would like
+  {
+    title: "A Game of Thrones",
+    tags: ["fantasy", "george r.r. martin"],
+  },
+  {
+    title: "Eloquent JavaScript",
+    tags: ["technology", "programming", "marijn haverbeke"],
+  },
+  {
+    title: "The Fellowship of the Ring",
+    tags: ["fantasy", "jrr tolkien"],
+  },
+  {
+    title: "The Return of the King",
+    tags: ["fantasy", "jrr tolkien"],
+  },
+  {
+    title: "The Anthropocene Reviewed",
+    tags: ["nonfiction", "john green"],
+  },
+  {
+    title: "The Left Hand of Darkness",
+    tags: ["sci-fi", "ursula le guin "],
+  },
+  // add more books if you would like
 ];
 
 function search(tag) {
-    // your code here
+  return library.filter((book) => book.tags.includes(tag));
 }
 
 console.log(search("fantasy"));
